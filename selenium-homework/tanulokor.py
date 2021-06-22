c8eef190d7750be1b06d418ef92ff21f562f4269
Szabolcs Petkes <sz.petkes@gmail.com>
2021-06-22 20:22:34 +0200

    Create tanulokor.py

---
+++ selenium-homework/tanulokor.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver.get("http://localhost:9999/trickyelements.html")
+
