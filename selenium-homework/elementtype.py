be6c843224659b7c5f849f5f239a01425f689f52
Szabolcs Petkes <sz.petkes@gmail.com>
2021-06-29 19:07:01 +0200

    Update elementtype.py

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -37,3 +37,20 @@ for lista_elem in element_lista:
         continue
     else:
         print("A szöveg helytelen.")
+        
+# try:
+#    driver.get("http://localhost:9999/trickyelements.html")
+#    found_elements = [driver.find_element_by_id("element1"), driver.find_element_by_id("element2"), driver.find_element_by_id("element3"),
+#                      driver.find_element_by_id("element4"), driver.find_element_by_id("element5")]
+#    for element in found_elements:
+#        if element.tag_name == "button" or element.get_attribute("onclick") is not None:
+#            button_text = element.text
+#            element.click()
+#            break
+#    message_text = driver.find_element_by_id("result").text
+#    assert f'{button_text} was clicked' == message_text
+#    print("test finished OK")
+#
+# except NoSuchElementException as e:
+#    print('Element not found: ', e)
+#     driver.close()
