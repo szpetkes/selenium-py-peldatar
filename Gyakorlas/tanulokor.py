50686761496ded6c6490506f61e4bc64a07483de
Szabolcs Petkes <sz.petkes@gmail.com>
2021-07-09 11:53:17 +0200

    Update tanulokor.py

--- Gyakorlas/tanulokor.py
+++ Gyakorlas/tanulokor.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -7,6 +8,11 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-driver.get("http://localhost:9999/trickyelements.html")
+driver.get("https://happy-sea-0a5ffef03.azurestaticapps.net/masodik.html")
 
-
+driver.get("https://happy-sea-0a5ffef03.azurestaticapps.net/masodik.html")
+driver.find_element(By.XPATH, "//div[@id=\'container\']/input").click()
+driver.find_element(By.XPATH, "//div[@id=\'container\']/input").send_keys("new todo")
+driver.find_element(By.XPATH, "//div[@id=\'container\']/ul/li").click()
+driver.find_element(By.XPATH, "//i[@id=\'plus-icon\']").click()
+driver.find_element(By.XPATH, "//div[@id=\'container\']/ul/li/span/i").click()
