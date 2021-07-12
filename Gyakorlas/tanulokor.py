346c617fdf06d216c8f8882cc6057e4ea61a1c23
Szabolcs Petkes <sz.petkes@gmail.com>
2021-07-12 20:12:52 +0200

    Házi feladat

---
+++ Gyakorlas/tanulokor.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver.get("")
