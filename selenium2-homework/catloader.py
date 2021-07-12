fbe12f5746d60de15b1cb496e93b5902a382174d
Szabolcs Petkes <sz.petkes@gmail.com>
2021-07-12 20:12:01 +0200

    Házi feladat

--- selenium2-homework/catloader.py
+++ selenium2-homework/catloader.py
@@ -5,6 +5,38 @@ oldalról. A fájlokat egy külön cats könyvtárba mentsd le. A fájlneve legy
 sorszám alatt azt értjük, hogy hanyadiknak lett megjelenítve és cat_id meg az azonosító amit szolgáltató ad. A {} 
 jelek ne legyenek benne a fájlnévben. 
 A megoldást egy catloader.py nevű fileban kell beadnod.
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import requests
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+driver = webdriver.Chrome(ChromeDriverManager().install())
+URL = "https://gentle-bay-0e4e13803.azurestaticapps.net/loadmore.html"
+driver.get(URL)
+
+time.sleep(3)
+load_more_button = driver.find_element_by_tag_name("button")
+for _ in range(2):
+    load_more_button.click()
+    time.sleep(3)
+cats = driver.find_elements_by_tag_name("img")
+
+print(cats)
+
+for index, cat in enumerate(cats):
+    url_str = cat.get_attribute("src")
+    cat_id = cat.find_element_by_xpath("../p").text.replace("Cat id: ", "")
+    cat_file_name = f"cats/{index}_{cat_id}"
+    r = requests.get(url_str)
+    if r.status_code == 200:
+        with open(cat_file_name, 'wb') as f:
+            f.write(r.content)
+
+print()
+print(len(cats))
+
 """""
 import time
 
@@ -14,7 +46,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://localhost:9999/loadmore.html")
 load_more = driver.find_element_by_xpath("//div[@class='load-more-button']/button")
-oldal = 5
+oldal = 3
 
 local_file = 'c:\\cats'
 cat_list = []
