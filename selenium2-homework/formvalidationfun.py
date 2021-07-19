4ed983d4d8bd8b1a03e44f75d58736d53a51f736
Szabolcs Petkes <sz.petkes@gmail.com>
2021-07-19 15:06:02 +0200

    Update formvalidationfun.py

--- selenium2-homework/formvalidationfun.py
+++ selenium2-homework/formvalidationfun.py
@@ -4,3 +4,27 @@ program töltse be a példatárból az http://localhost:9999/simplevalidation.ht
 az űlap mező ellőnőrző funkcióit. 
 A megoldást egy formvalidationfun.py nevű fileban kell beadnod.
 """""""""""
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("http://localhost:9999/simplevalidation.html")
+driver.find_element_by_id("test-email").send_keys("testemail")
+
+input1 = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='e-w11uh9t9']"))).get_attrubite("validationMessage")
+assert input1 is None
+assert input1 == "Please check your E-Mail format"
+
+driver.find_element_by_name("Password").click()
+input2 = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='e-w11uh9t9']"))).get_attrubite("validationMessage")
+
+assert input2 is None
+assert input2 == "This field can't be empty"
+
+driver.find_element_by_id("e-ami9ay1w").click()
