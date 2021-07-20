6f90c68e0a3467bd0ec58967120f91ddd7e037fe
Szabolcs Petkes <sz.petkes@gmail.com>
2021-07-20 14:05:24 +0200

    Házi feladat

--- mypytest/mypytest.py
+++ mypytest/mypytest.py
@@ -62,3 +62,41 @@ https://www.geeksforgeeks.org/object-oriented-testing-in-python/
     A megoldást egy mypytest.py nevű fileban kell beadnod. Lehetőleg külön mappában, hiszen ez egy teljes projek.
 
 """""""""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pytest
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+
+@pytest.fixture(scope='session')
+def browser():
+    driver = webdriver.Chrome()
+    driver.get("https://react-card-2a6c5.web.app/")
+    return driver
+
+
+def test_first_testcase(browser):
+    yes_element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'yes')))
+    yes_element.click()
+    browser.find_element_by_id("buttoncheck").click()
+    p = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@id="container"]/p')))
+    print(p.text)
+
+
+def test_second_testcase(browser):
+    browser.get("https://google.com")
+    time.sleep(2)
+    assert True
+
+
+driver.quit()
